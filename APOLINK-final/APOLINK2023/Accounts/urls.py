22b250dce9b3f01b6f2c5ced5414d68b69a39d46
Alebrex99 <ale.brex99@gmail.com>
2023-04-06 14:25:38 +0300

    start edit User profile

--- APOLINK-final/APOLINK2023/Accounts/urls.py
+++ APOLINK-final/APOLINK2023/Accounts/urls.py
@@ -9,6 +9,7 @@ app_name = 'Accounts'
 urlpatterns = [
     path('signup/', views.Signup, name='signup'),
     path('activate/<uidb64>/<token>', views.Activate, name ='activate'),
+    path('EditProfile/', views.EditProfile, name='editProfile'),
     path('login/', views.CustomLoginView.as_view(template_name="Accounts/login.html"), name='login'),
     #path('login/', views.user_login, name='login'), #DEBUG
     path('logout/', views.user_logout, name='logout'),
